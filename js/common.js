1d753bcc257ad751f4ecd7bedc9a8af5c2b1c490
GAYOUNG914 <rkdud91455@gmail.com>
2021-06-19 23:54:11 +0900

    docs: fix codes
    fix codes for interaction

--- js/common.js
+++ js/common.js
@@ -1,58 +1,64 @@
-$(function(){
+$(function () {
   /*script 영역 count animation 사용 예*/
   //$(".flex_box .count").attr('title','http://naver.com');
   // $(".flex_box .count").prop('Counter',0);
   // console.log($(".flex_box .count").prop("Counter"))
 
-  $(".flex_box .count").each(function(i){
+  $(".flex_box .count").each(function (i) {
     // if( i == 0 ){
     //   $(this).prop("Counter",0)
     // }else{
     //   $(this).prop("Counter",1)
     // }
-    $(this).prop("Counter",0).animate({Counter: $(this).text()},{
-      duration: 4000,
-      easing: 'swing',
-      step: function(now){
-        $(this).text(Math.ceil(now))
-      }
-    });
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        { Counter: $(this).text() },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
   });
+  //숫자 나타내기
 
-  
+  //back to top
   //script 입력영역
-  $("nav ul li a,.back_to_top a,a.button,.footer_top a").click(function(){
-    console.log($(this.hash))
+  $(".back_to_top a").click(function () {
+    console.log($(this.hash));
     let thisElem = $(this.hash);
     let offsetElem = thisElem.offset();
-    console.log(offsetElem.top)
+    console.log(offsetElem.top);
     $("html,body").stop();
-    $("html,body").animate({scrollTop : offsetElem.top},1500);
+    $("html,body").animate({ scrollTop: offsetElem.top }, 1500);
   });
 
   //scroll 시 하단 이동버튼 노출/비노출
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     // console.log($(this).scrollTop())
-    if( $(this).scrollTop() == 0 ){
+    if ($(this).scrollTop() == 0) {
       $(".back_to_top").removeClass("on");
-    }else{
+    } else {
       $(".back_to_top").addClass("on");
     }
   });
 
+  //shpw hide class 추가
   //section offset top 값으로 class 추가
-  const addTop = 500;
+  const addTop = 900;
 
   let wHeight = $(window).innerHeight();
-  console.log("window height: "+wHeight)
-  $(window).scroll(function(){
+  console.log("window height: " + wHeight);
+  $(window).scroll(function () {
     let thisScrollTop = $(this).scrollTop();
-    $("section").each(function(){
+    $("section").each(function () {
       let thisOffset = $(this).offset();
-      if( thisOffset.top <= thisScrollTop + addTop && thisScrollTop < thisOffset.top + wHeight ){
+      if (thisOffset.top <= thisScrollTop + addTop && thisScrollTop < thisOffset.top + wHeight) {
         $(this).addClass("active");
       }
     });
-  });;
-
-});//document ready
+  });
+}); //document ready
